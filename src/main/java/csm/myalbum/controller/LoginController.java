1ed2559f7973610102cdd2caa085aee971df362c
chrhfoq123 <chrhfoq123@naver.com>
2023-11-25 20:11:11 +0900

    loginForm 퍼블리싱

---
+++ src/main/java/csm/myalbum/controller/LoginController.java
@@ -0,0 +1,18 @@
+package csm.myalbum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("login")
+    public String login(){
+        return "loginForm";
+    }
+
+    @GetMapping("addMember")
+    public String addMember(){
+        return "addMemberForm";
+    }
+}
